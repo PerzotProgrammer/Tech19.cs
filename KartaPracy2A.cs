636636198ad66c3f13a15b09b64be9b97a21fe7f
PerzotProgramer <bartoszperz2007@gmail.com>
2022-10-14 14:08:52 +0200

    Zwiękrzono czytelność kodu + oznakowanie liczb

--- KartaPracy2A.cs
+++ KartaPracy2A.cs
@@ -7,22 +7,29 @@ namespace KartaPracy2A
             int a,b,c,d;
             //zad1
             System.Console.WriteLine("ZADANIE 1");
+            System.Console.Write("Liczba1: ");
             a = int.Parse(System.Console.ReadLine());
+            System.Console.Write("Liczba2: ");
             b = int.Parse(System.Console.ReadLine());
             if ((a + b) % 2 == 0) System.Console.WriteLine("TAK, suma parzysta"); 
             else System.Console.WriteLine("NIE, suna nieparzysta");
 
             //zad2
             System.Console.WriteLine("ZADANIE 2");
+            System.Console.Write("Liczba1: ");
             a = int.Parse(System.Console.ReadLine());
+            System.Console.Write("Liczba2: ");
             b = int.Parse(System.Console.ReadLine());
             if ((a + b)/ 2 > Math.Sqrt(a * b)) System.Console.WriteLine("TAK śr ar > geo");
             else System.Console.WriteLine("NIE śr ar < geo");
 
             //zad3
             System.Console.WriteLine("ZADANIE 3");
+            System.Console.Write("Liczba1: ");
             a = int.Parse(System.Console.ReadLine());
+            System.Console.Write("Liczba2: ");
             b = int.Parse(System.Console.ReadLine());
+            System.Console.Write("Liczba3: ");
             c = int.Parse(System.Console.ReadLine());
             if (a == b || a == c || b == c){
                 System.Console.WriteLine("TAK, są przynajmniej dwie liczby równe siebie");
@@ -34,9 +41,13 @@ namespace KartaPracy2A
 
             //zad4
             System.Console.WriteLine("ZADANIE 4");
+            System.Console.Write("Liczba1: ");
             a = int.Parse(System.Console.ReadLine());
+            System.Console.Write("Liczba2: ");
             b = int.Parse(System.Console.ReadLine());
+            System.Console.Write("Liczba3: ");
             c = int.Parse(System.Console.ReadLine());
+            System.Console.Write("Liczba4: ");
             d = int.Parse(System.Console.ReadLine());
             if (a < b && a < c && a < d) System.Console.WriteLine("Najmniejsza to " + a);
             else if (b < a && b < c && b < d) System.Console.WriteLine("Najmniejsza to " + b);
@@ -46,8 +57,11 @@ namespace KartaPracy2A
             
             //zad5
             System.Console.WriteLine("ZADANIE 5");
+            System.Console.Write("Liczba1: ");
             a = int.Parse(System.Console.ReadLine());
+            System.Console.Write("Liczba2: ");
             b = int.Parse(System.Console.ReadLine());
+            System.Console.Write("Liczba3: ");
             c = int.Parse(System.Console.ReadLine());
             if (b - c < a && a < b + c || a - c < b && b < a + c || a - b < c && c < a + b)
             { 
@@ -57,21 +71,24 @@ namespace KartaPracy2A
 
             //zad6
             System.Console.WriteLine("ZADANIE 6");
+            System.Console.Write("Liczba1: ");
             a = int.Parse(System.Console.ReadLine());
+            System.Console.Write("Liczba2: ");
             b = int.Parse(System.Console.ReadLine());
+            System.Console.Write("Liczba3: ");
             c = int.Parse(System.Console.ReadLine());
             if (a < b + c && b < a + c && c < a + b)
             {
                 System.Console.WriteLine("Trójkąt powstanie");
-                if (a**2 + b**2 == c**2 || b**2 + c**2 == a**2 || c**2 + a**2 == b**2)
+                if (a*a + b*b == c*c || b*b + c*c == a*a || c*c + a*a == b*b)
                 {
                     System.Console.WriteLine("Będzie to trójkąt prostokątny");
                 }
-                else if (a**2 + b**2 < c**2 || b**2 + c**2 < a**2 || c**2 + a**2 < b**2)
+                else if (a*a + b*b < c*c || b*b + c*c < a*a || c*c + a*a < b*b)
                 {
                     System.Console.WriteLine("Będzie to trójkąt rozwartokątny");
                 }
-                else if (a**2 + b**2 > c**2 || b**2 + c**2 > a**2 || c**2 + a**2 > b**2)
+                else if (a*a + b*b > c*c || b*b + c*c > a*a || c*c + a*a > b*b)
                 {
                     System.Console.WriteLine("Będzie to trójkąt ostrokątny");
                 }
