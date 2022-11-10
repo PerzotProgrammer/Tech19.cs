8ca44bb5e70a09198a3500fcc0e8bbb6e4d4ea56
PerzotProgramer <bartoszperz2007@gmail.com>
2022-11-11 00:42:57 +0100

    Wykonanie zadań z spr

---
+++ SPR1gr1.cs
@@ -0,0 +1,59 @@
+﻿Console.WriteLine("ZADANIE 1");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+int d = int.Parse(Console.ReadLine());
+
+Console.Write("a: ");
+Console.WriteLine((a+b+c+d)/2f);
+
+Console.Write("b: ");
+if (a - 2 == d || d - 2 == a) Console.Write(true);
+else Console.WriteLine(false);
+
+Console.Write("c: ");
+int spr = 0;
+if (a < b) spr++;
+if (b < c) spr++;
+if (c < d) spr++;
+Console.WriteLine(spr);
+
+Console.WriteLine("ZADANIE 2 NIE DZIAŁA!");
+int i = 0; 
+int col = 0;
+
+for (col = 10; col < 100; col++) 
+{
+    int temp = col;
+    i = 0;
+    for(temp = i; temp > 1; i++)
+    {
+        if(temp % 2 == 0)
+        {
+            temp /= 2;
+        }
+        else
+        {
+            temp = (temp * 3) + 1;
+        }
+    }
+    if (i == 9 && temp == 1)
+    {
+        Console.WriteLine($"1 na 9 miejscu jest kiedy n = {col}");
+        break;
+    }
+}
+
+int x = 7;
+Console.WriteLine("ZADANIE 3");
+
+for(int i = 1; i <= x; i++)
+{
+    for(int j = 1; j <= x; j++)
+    {
+        if(i-1 == x/2 || j-1 == x/2) Console.Write("*");
+        else if(i == j || j == i || i + j == x+1 || j + i == x+1) Console.Write("#");
+        else Console.Write(" ");
+    }
+    Console.WriteLine();
+}
---
+++ SPR1gr2.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine("ZADANIE 1");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+Console.Write("a: ");
+if (c == b - a || c * 2 == b - a || c * 3 == b - a || c * 4 == b - a || c * 5 == b - a) Console.WriteLine(true);
+else Console.WriteLine(false);
+
+Console.Write("b: ");
+if (c >= b - a) Console.WriteLine(1);
+else if (c * 2 >= b - a) Console.WriteLine(2);
+else if (c * 3 >= b - a) Console.WriteLine(3);
+else if (c * 4 >= b - a) Console.WriteLine(4);
+else if (c * 5 >= b - a) Console.WriteLine(5);
+else Console.WriteLine(false);
+
+Console.Write("c: ");
+if (c * 5 == b - a) Console.WriteLine(true);
+else Console.WriteLine(false);
+
+Console.WriteLine("ZADANIE 2");
+
+for (int faj = 1000; faj <= 10000; faj++)
+{
+    int jed = faj % 10;
+    int dz = (faj % 100)/10;
+    int set = (faj % 1000)/100;
+    if (jed * 2 > dz && dz * 2 > set) Console.WriteLine(faj);
+}
+
+Console.WriteLine("ZADANIE 3");
+int x = 7;
+
+for (int i = 1; i <= x; i++)
+{
+    for (int j = 1; j <= x; j++)
+    {
+        if (i == j || i + j == x + 1 || i - 1 == j || j - 1 == i || i + j == x + 2 || i + j == x) Console.Write("*");
+        else Console.Write(" ");
+    }
+    Console.WriteLine();
+}
