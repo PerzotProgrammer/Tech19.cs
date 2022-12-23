3019f9b41634d39f450f67eb5a21f8b9f5a6bb0c
Perzot Programista <bartoszperz2007@gmail.com>
2022-12-23 16:40:30 +0100

    Zmieniono #pragma

--- KartyPracy/KartaPracy6.cs
+++ KartyPracy/KartaPracy6.cs
@@ -1,5 +1,4 @@
-﻿#pragma warning disable CS0168 // Variable is declared but never used
-#pragma warning disable CS0219 // Variable is assigned but its value is never used
+﻿#pragma warning disable
 float suma = 0;
 int dzielnik = 0;
 int liczba = 0;
