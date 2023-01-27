73a073845ff07ebd6b81bf38d35e4e42ff666539
Perzot Programista <bartoszperz2007@gmail.com>
2023-01-27 20:06:02 +0100

    Dla Miłosza

---
+++ KartyPracy/cs.java
@@ -0,0 +1,11 @@
+public class cs {
+    public static void main(String[] args) throws Exception {
+        long ile = 1;
+        while(ile < 2147483647)
+        {
+            System.out.print("C# to gówno!\nMiłosz reluje po raz ");
+            System.out.println(ile);
+            ile++;
+        }
+    }
+}
