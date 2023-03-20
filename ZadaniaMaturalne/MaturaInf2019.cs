43464b9fa01edea8d32ef9c3de5ccfd7910ec6d4
Perzot Programista <bartoszperz2007@gmail.com>
2023-03-20 15:21:23 +0100

    Przesłanie zadań z lekcji

---
+++ ZadaniaMaturalne/MaturaInf2019.cs
@@ -0,0 +1,25 @@
+using System.IO;
+using System;
+
+// zad 4
+
+StreamReader liczby = new StreamReader("MaturaInf2019/liczby.txt");
+StreamWriter wpiszLiczby = new StreamWriter("MaturaInf2019/wyniki4_1.txt");
+
+
+bool CzyPierwsza(int x){
+    for (int i = 2; i < x; i++){
+        if (x % i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+while(!liczby.EndOfStream){
+    int spr = Convert.ToInt32(liczby.ReadLine());
+    if (CzyPierwsza(spr) && spr <= 5000 && spr >= 100) wpiszLiczby.WriteLine(spr);
+}
+
+wpiszLiczby.Close();
