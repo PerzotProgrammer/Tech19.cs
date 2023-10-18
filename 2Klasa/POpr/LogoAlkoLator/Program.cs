a2ddd9ab68bcee1cc2e9d5a0b74c2d6aed5610ba
PerzotProgrammer <bartoszperz2007@gmail.com>
2023-10-18 17:10:24 +0200

    Przesłano projekt z lekcji

---
+++ 2Klasa/POpr/LogoAlkoLator/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace LogoAlkoLator;
+
+class Program
+{
+    static void Main()
+    {
+        string? user = Logowanie();
+        if (user != "niezalogowany") Trzezwosc(user);
+    }
+    
+    // SYSTEM LOGOWANIA
+    static string? Logowanie()
+    {
+        Console.Write("Podaj nazwę użytkownika: ");
+        string? user = Console.ReadLine();
+        Console.Write("Podaj hasło użytkownika: ");
+        string? passwd = Console.ReadLine();
+
+        string?[] UsersDB = new string[100];
+        string?[] PasswdsDB = new string[100];
+        
+        StreamReader Users = new("users.txt");
+        int index = 0;
+        while (!Users.EndOfStream)
+        {
+            UsersDB[index] = Users.ReadLine();
+            index++;
+        }
+
+        index = 0;
+        StreamReader Passwds = new("passwds.txt");
+        while (!Passwds.EndOfStream)
+        {
+            PasswdsDB[index] = Passwds.ReadLine();
+            index++;
+        }
+
+        bool CzyZalogowany = false;
+        
+        for (int i = 0; i < UsersDB.Length; i++)
+        {
+            if(user == UsersDB[i] && passwd == PasswdsDB[i])
+            {
+                Console.WriteLine("Logowanie udane!");
+                CzyZalogowany = true;
+                return user;
+            }
+        }
+
+        Console.WriteLine("Logowanie nieudane!");
+        return "niezalogowany";
+    }
+
+    // NIE DZIAŁA
+    // Sprawdzanie trzeźwości
+    static bool Trzezwosc(string? user)
+    {
+        Random Rand = new();
+        double promil = Rand.NextDouble();
+
+    }
+}
---
+++ 2Klasa/POpr/LogoAlkoLator/readme.md
@@ -0,0 +1,19 @@
+# LOGOALKOLATOR
+## Dokumentacja
+### 1 - Planowanie systemu
+Logoalkolator to program umożliwający logowanie i obliczanie liczb osobie trzeźwej
+### 2 - Części składowe
+- system logowania
+- sprawdzanie trzeźwości
+- obliczanie
+- wylogowanie
+
+### 3 - Projekt systemu
+przykład użycia:
+- user się loguje
+- user rozwiązuje zadanie matematyczne
+- user może wykonać dowolną operacje
+
+Baza danych na razie stała w pliku baza.txt
+
+#### Btw jestem leniem
