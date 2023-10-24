8ea31e42ebae6df8aec724e32568b4569409ef5d
PerzotProgrammer <bartoszperz2007@gmail.com>
2023-10-24 12:20:35 +0200

    Przesłano zadania z lekcji

---
+++ 2Klasa/POb/TryCatch/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace TryCatch;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.WriteLine("Podaj dwie liczby:");
+            Console.Write("Liczba a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Liczba b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int wynik = a / b;
+            Console.WriteLine($"Wynik: {a} / {b} = {wynik:F4}");
+        }
+        catch (DivideByZeroException)
+        {
+            ErrorMessage("Nie można dzielić przez 0!");
+        }
+        catch (FormatException)
+        {
+            ErrorMessage("Nie można przekonwertować na liczbę!");
+        }
+        catch (OverflowException)
+        {
+            ErrorMessage($"Liczba jest za duża!\nDostępny zakres: {int.MinValue} - {int.MaxValue}");
+        }
+        catch (Exception e)
+        {
+            ErrorMessage($"Wystąpił inny niezdefiniowany błąd.\n{e.Message}");
+        }
+    }
+    
+    static void ErrorMessage(string errorMessage)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"Błąd: {errorMessage}");
+        Console.ResetColor();
+    }
+}
