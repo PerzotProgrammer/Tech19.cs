c647d7a10af59da219e42189826ba43af8c72d3c
PerzotProgrammer <bartoszperz2007@gmail.com>
2023-11-04 23:42:38 +0100

    Przesłano projekty treningowe

---
+++ MojeProjekty/ToDoCMD/Program.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+
+namespace ToDoCMD;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bool loop = true;
+
+        Console.WriteLine("Menadżer Zadań");
+        Console.WriteLine();
+
+        while (loop)
+        {
+            Console.WriteLine("Wybierz opcje:");
+            Console.WriteLine("w - wyświetl zadania");
+            Console.WriteLine("d - dodaj zadania");
+            Console.WriteLine("e - edytuj zadania");
+            Console.WriteLine("q - wyjdź");
+            Console.WriteLine();
+            Console.Write("Twoja opcja: ");
+
+            char option = Console.ReadKey().KeyChar;
+
+            switch (option)
+            {
+                case 'q':
+                    Console.Clear();
+                    Exit();
+                    break;
+
+                case 'w':
+                    Wczytaj();
+                    break;
+
+                case 'd':
+                    Dodaj(); 
+                    break;
+
+                case 'e':
+                    Edytuj();
+                break;
+
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Zły argument!");
+                    Console.WriteLine();
+                    break;
+            }
+        }
+
+
+        bool Exit() { return loop = false; }
+
+        void Wczytaj()
+        {
+            StreamReader sr = new StreamReader("./data.txt");
+
+            Console.Clear();
+            while (!sr.EndOfStream)
+            {
+                Console.WriteLine(sr.ReadLine());
+            }
+            sr.Close();
+            Console.WriteLine();
+        }
+
+        void Dodaj()
+        {
+            StreamReader sr = new StreamReader("./data.txt");
+            int numOfLines = 0;
+            while (!sr.EndOfStream)
+            {
+                sr.ReadLine();
+                numOfLines++;
+            }
+            sr.Close();
+
+            StreamWriter sw = new StreamWriter("./data.txt", true);
+
+            Console.Clear();
+            
+            Console.WriteLine("Podaj zadanie do dodania:");
+            string msg = Convert.ToString($"{numOfLines} - {Console.ReadLine()}");
+            sw.WriteLine(msg);
+            sw.Close();
+            Console.Clear();
+        }
+
+        void Edytuj()
+        {
+            Console.Clear();
+            Console.WriteLine("Czy chcesz nadpisać wszystkie dane: t/n");
+            char option = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            switch (option)
+            {
+                case 't':
+                    StreamWriter sw1 = new StreamWriter("./data.txt");
+
+                    sw1.WriteLine();
+                    sw1.Close();
+                    break;
+
+                case 'n':
+                    Console.WriteLine("Wybierz linię:");
+                    int line = Convert.ToInt32(Console.ReadLine());
+                    StreamReader sr = new StreamReader("./data.txt");
+                    ArrayList dataLine = new ArrayList();
+                    while (!sr.EndOfStream)
+                    {
+                        dataLine.Add(sr.ReadLine());
+                    }
+
+                    sr.Close();
+                    Console.WriteLine("Podaj zadanie do dodania:");
+                    string wiadomosc = Convert.ToString($"{line} - {Console.ReadLine()}");
+                    StreamWriter sw2 = new StreamWriter("./data.txt");
+
+                    for (int i = 0; i < dataLine.Count; i++)
+                    {
+                        if (i == line)
+                        {
+                            sw2.WriteLine(wiadomosc);
+                        }
+                        else
+                        {
+                            sw2.WriteLine(dataLine[i]);
+                        }
+                    }
+                    sw2.Close();
+                    Console.Clear();
+                    break;
+
+                default:
+                    Console.Clear() ;
+                    Console.WriteLine("Zły argument!");
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+
+            }
+        }
+    }
+}
---
+++ MojeProjekty/WpfCalculator/MainWindow.xaml.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Data;
+using System.Windows;
+using System.Windows.Input;
+
+
+namespace WpfCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        string aktWynik = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void mouseClick(object sender, MouseEventArgs e)
+        {
+            // POBIERANIE AKUTALNEGO ELEMENTU
+
+            string? elementName = null;
+            var mouseWasDownOn = e.Source as FrameworkElement;
+            if (mouseWasDownOn != null && mouseWasDownOn.Name != "textBlock")
+            {
+                elementName = mouseWasDownOn.Name;
+                textBlock.Text = elementName;
+            }
+
+            // LOGIKA PRZYCISKÓW
+
+            switch (elementName)
+            {
+                case "b0":
+                    aktWynik += "0";
+                    textBlock.Text = aktWynik;
+                    break;
+
+                case "b1":
+                    aktWynik += "1";
+                    textBlock.Text = aktWynik;
+                    break;
+
+                case "b2":
+                    aktWynik += "2";
+                    textBlock.Text = aktWynik;
+                    break;
+
+                case "b3":
+                    aktWynik += "3";
+                    textBlock.Text = aktWynik;
+                    break;
+
+                case "b4":
+                    aktWynik += "4";
+                    textBlock.Text = aktWynik;
+                    break;
+
+                case "b5":
+                    aktWynik += "5";
+                    textBlock.Text = aktWynik;
+                    break;
+
+                case "b6":
+                    aktWynik += "6";
+                    textBlock.Text = aktWynik;
+                    break;
+
+                case "b7":
+                    aktWynik += "7";
+                    textBlock.Text = aktWynik;
+                    break;
+
+                case "b8":
+                    aktWynik += "8";
+                    textBlock.Text = aktWynik;
+                    break;
+
+                case "b9":
+                    aktWynik += "9";
+                    textBlock.Text = aktWynik;
+                    break;
+
+                case "bc":
+                    aktWynik += ".";
+                    textBlock.Text = aktWynik;
+                    break;
+
+                // DZIAŁANIA
+
+                case "bd":
+                    aktWynik += "+";
+                    textBlock.Text = aktWynik;
+                    break;
+
+                case "bo":
+                    aktWynik += "-";
+                    textBlock.Text = aktWynik;
+                    break;
+
+                case "bpo":
+                    aktWynik += "*";
+                    textBlock.Text = aktWynik;
+                    break;
+
+                case "bpd":
+                    aktWynik += "/";
+                    textBlock.Text = aktWynik;
+                    break;
+
+                // EWALUACJA
+
+                case "bwy":
+                    if (aktWynik == "")
+                    {
+                        textBlock.Text = "0";
+                    }
+                    else
+                    {
+                        try
+                        {
+                            DataTable dt = new();
+                            var v = dt.Compute(aktWynik, "");
+                            string? output = v.ToString();
+                            if (output == "NaN" || output == "\u221E") throw new DivideByZeroException();
+                            textBlock.Text = output;
+                        }
+                        catch (DivideByZeroException)
+                        {
+                            textBlock.Text = "NIE MOŻNA DZIELIĆ PRZEZ 0!";
+                        }
+                        catch (Exception)
+                        {
+                            textBlock.Text = "NIE POPRAWNE DZIAŁANIE";
+                        }
+                        finally
+                        { 
+                            aktWynik = "";
+                        }
+                    }
+
+                    break;
+            }
+        }
+    }
+}
