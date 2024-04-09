befe70ae8125343b0773dff3c23fef355359be0c
PerzotProgrammer <bartoszperz2007@gmail.com>
2024-04-09 12:18:03 +0200

    Przesłano zadanie z lekcji

---
+++ 2Klasa/POb/Destruktory/Class/Car.cs
@@ -0,0 +1,57 @@
+namespace Destruktory.Class;
+
+public class Car
+{
+    private string Brand;
+    private string Model;
+    private bool IsDamaged;
+    private string DamageType;
+
+    public Car(string brand, string model)
+    {
+        Brand = brand;
+        Model = model;
+        IsDamaged = false;
+        DamageType = "";
+    }
+
+    public void RandomDamage()
+    {
+        Random random = new();
+        int rand = random.Next(1, 10);
+        if (rand == 1)
+        {
+            IsDamaged = true;
+            DamageType = "przebitą oponę";
+            Console.WriteLine("Uszkodziłeś oponę!");
+        }
+        else if (rand == 2)
+        {
+            IsDamaged = true;
+            DamageType = "zatarty silnik";
+            Console.WriteLine("Zatarłeś silnik!");
+        }
+    }
+
+    public string GetName()
+    {
+        return $"{Brand} {Model}";
+    }
+
+    public void Drive()
+    {
+        if (IsDamaged)
+        {
+            Console.WriteLine($"Samochód {GetName()} ma {DamageType} i nie może jechać!");
+            return;
+        }
+
+        Console.WriteLine($"Samochód {GetName()} jedzie!");
+        RandomDamage();
+    }
+
+    ~Car() // Trochę to nie działa
+    {
+        Console.WriteLine($"Samochód {Brand} {Model} się zniszczył");
+    }
+}
---
+++ 2Klasa/POb/Destruktory/Program.cs
@@ -0,0 +1,97 @@
+﻿using Destruktory.Class;
+
+namespace Destruktory;
+
+class Program
+{
+    static Dictionary<int, Car> CarsDict = new();
+    static bool LoopFlag = true;
+
+    static void Main()
+    {
+        Console.WriteLine("Symulator jazdy samochodem");
+        while (LoopFlag)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Wybierz opcję:");
+            Console.WriteLine("1. Stwórz samochód");
+            Console.WriteLine("2. Wyświetl listę");
+            Console.WriteLine("3. Jedź samochodem");
+            Console.WriteLine("4. Zniszcz samochód");
+            Console.WriteLine("5. Wyjdź z programu");
+            string? chk = Console.ReadLine();
+            switch (chk)
+            {
+                case "1":
+                    Car car = CreateCar();
+                    Console.WriteLine($"Dodano samochód {car.GetName()}");
+                    CarsDict.Add(CarsDict.Count + 1, car);
+                    break;
+                case "2":
+                    ShowCars();
+                    break;
+                case "3":
+                    DriveSelectedCar();
+                    break;
+                case "4":
+                    DestroyCar();
+                    break;
+                case "5":
+                    LoopFlag = false;
+                    break;
+                default:
+                    Console.WriteLine("Niepoprawna operacja!");
+                    break;
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Dziękujemy za skorzystanie z tego programu");
+    }
+
+    static Car CreateCar()
+    {
+        Console.Write("Marka: ");
+        string brand = Console.ReadLine()!;
+        Console.Write("Model: ");
+        string model = Console.ReadLine()!;
+        Car car = new(brand, model);
+        return car;
+    }
+
+    static void ShowCars()
+    {
+        Console.WriteLine("Lista samochodów:");
+        foreach (KeyValuePair<int, Car> pair in CarsDict) Console.WriteLine($"{pair.Key}: {pair.Value.GetName()}");
+    }
+
+    static void DriveSelectedCar()
+    {
+        try
+        {
+            Console.Write($"Wybierz samochód(1 - {CarsDict.Count}): ");
+            int select = int.Parse(Console.ReadLine()!);
+            CarsDict[select].Drive();
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("NIEPOPRAWNE DANE!");
+        }
+    }
+
+    static void DestroyCar()
+    {
+        try
+        {
+            Console.Write($"Wybierz samochód(1 - {CarsDict.Count}): ");
+            int select = int.Parse(Console.ReadLine()!);
+            CarsDict.Remove(select);
+            Console.WriteLine("Samochód został zniszczony");
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("NIEPOPRAWNE DANE!");
+        }
+    }
+}
