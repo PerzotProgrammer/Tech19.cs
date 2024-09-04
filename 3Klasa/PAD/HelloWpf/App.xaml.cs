eaf50bde5edaab82a13518b053f7645e5d1950e8
PerzotProgrammer <bartoszperz2007@gmail.com>
2024-09-04 14:38:49 +0200

    Przesłano zadanie z lekcji

---
+++ 3Klasa/PAD/HelloWpf/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace HelloWpf;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
