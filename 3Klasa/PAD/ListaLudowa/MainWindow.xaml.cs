05a37f6b9762443ee3ef259d4cbd862f31d72bb6
PerzotProgrammer <bartoszperz2007@gmail.com>
2024-11-13 18:36:12 +0100

    Przesłano zadanie z lekcji

---
+++ 3Klasa/PAD/ListaLudowa/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ListaLudowa;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void OnContentLoad(object sender, RoutedEventArgs e)
+    {
+        StreamReader reader = new StreamReader("./../../../ListaLudowa.txt");
+        while (!reader.EndOfStream)
+        {
+            PersonList.Text += $"{reader.ReadLine()}\n";
+        }
+    }
+}
