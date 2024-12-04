0e72e7c6562cac12763ace727342c0702b242ba6
PerzotProgrammer <bartoszperz2007@gmail.com>
2024-12-04 13:11:01 +0100

    Przesłano zadanie z lekcji

---
+++ 3Klasa/PAD/BazaDanych/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Data.Sqlite;
+
+namespace BazaDanych;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        ReadData();
+    }
+
+    private void ReadData()
+    {
+        using SqliteConnection connection = new SqliteConnection("Data Source=database.db");
+
+        connection.Open();
+        SqliteCommand command = connection.CreateCommand();
+        command.CommandText = "SELECT * FROM Osoby";
+        SqliteDataReader reader = command.ExecuteReader();
+
+        while (reader.Read())
+        {
+            TextBlock tb = new TextBlock();
+            tb.Text += $"ID: {reader.GetInt32(0)}\t Imie: {reader.GetString(1)}\t Nazwisko: {reader.GetString(2)}\n";
+            tb.Padding = new Thickness(10);
+            tb.Margin = new Thickness(10);
+            tb.Background = Brushes.Gray;
+            Panel.Children.Add(tb);
+        }
+
+
+        connection.Close();
+    }
+}
